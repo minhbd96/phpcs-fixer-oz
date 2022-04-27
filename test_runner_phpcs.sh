ed4a5e66452277e9dce1f2c9d3c91e942ca95e96
minhbd <minhbd@pascaliaasia.com>
2022-04-27 15:41:31 +0700

    [ADD] Doublequoute for command

--- test_runner_phpcs.sh
+++ test_runner_phpcs.sh
@@ -4,6 +4,6 @@ read -p "Enter base branch(feature/VDxxx): " base
 read -p "Enter dest branch(feature/VDxxx): " dest
 
 [[ -z "$repo" ]] || [[ -z "$base" ]] || [[ -z "$dest" ]] && echo "Missing input" && exit;
-list_files_change=$(git -C /data/${repo} diff ${base}..${dest} --line-prefix="/data/${repo}/" --name-only)
+list_files_change=$(git -C /data/${repo} diff "${base}..${dest}" --line-prefix="/data/${repo}/" --name-only)
 [[ -z "$list_files_change" ]] && echo "Don't have any change with base branch" && exit;
 phpcs --standard=PSR2 $list_files_change
